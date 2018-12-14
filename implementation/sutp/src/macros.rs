9ee0aed0375005dddf3f1b05888d7b71714a3429
Moritz Gunz <moritz.gunz@gmail.com>
2018-12-14 13:14:37 +0100

    Statically reference macros used in macros

--- implementation/sutp/src/macros.rs
+++ implementation/sutp/src/macros.rs
@@ -11,12 +11,12 @@ macro_rules! assert_size {
 macro_rules! debug_log_assert {
     ($a:expr) => {
         if !$a {
-            debug!("assertion failed: {} was {}", stringify!($a), $a);
+            ::log::debug!("assertion failed: {} was {}", stringify!($a), $a);
         }
     };
     ($a:expr, $msg:expr) => {
         if !$a {
-            debug!($msg, stringify!($a), $a);
+            ::log::debug!($msg, stringify!($a), $a);
         }
     };
 }
@@ -24,12 +24,12 @@ macro_rules! debug_log_assert {
 macro_rules! debug_log_eq {
     ($a:expr, $b:expr) => {
         if $a != $b {
-            debug!("assertion failed: {} != {}", $a, $b);
+            ::log::debug!("assertion failed: {} != {}", $a, $b);
         }
     };
     ($a:expr, $b:expr, $msg:expr) => {
         if $a != $b {
-            debug!($msg, $a, $b);
+            ::log::debug!($msg, $a, $b);
         }
     };
 }
