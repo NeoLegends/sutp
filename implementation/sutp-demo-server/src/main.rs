33ebced5b662a75e791cbb6a729dc7fa3cc2b509
Moritz Gunz <moritz.gunz@gmail.com>
2018-12-17 14:15:54 +0100

    Make demo server to echo server

--- implementation/sutp-demo-server/src/main.rs
+++ implementation/sutp-demo-server/src/main.rs
@@ -1,7 +1,10 @@
 use env_logger;
 use futures::{Future, Stream};
 use sutp::SutpListener;
-use tokio::{self, io::read_to_end};
+use tokio::{
+    self,
+    io::{read_to_end, shutdown, write_all},
+};
 
 fn main() {
     env_logger::init();
@@ -10,13 +13,20 @@ fn main() {
     let fut = SutpListener::bind(&addr)
         .unwrap()
         .incoming()
-        .map(|(conn, _)| conn)
-        .buffer_unordered(10)
-        .and_then(|stream| read_to_end(stream, Vec::new()))
-        .for_each(|(_, buf)| {
-            println!("{}", String::from_utf8_lossy(&buf));
-            Ok(())
+        .map(|(conn, addr)| {
+            println!("Accepting a connection from {}", addr);
+
+            conn.and_then(|stream| read_to_end(stream, Vec::new()))
+                .inspect(move |(_, buf)| println!(
+                    "Got '{}' from {}. Echoing back...",
+                    String::from_utf8_lossy(&buf),
+                    addr,
+                ))
+                .and_then(|(stream, buf)| write_all(stream, buf))
+                .and_then(|(stream, _)| shutdown(stream))
         })
+        .buffer_unordered(10)
+        .for_each(|_| Ok(()))
         .map_err(|e| panic!("err: {:?}", e));
 
     tokio::run(fut);
